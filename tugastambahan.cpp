166e30605a571df5875198e84c60e1d705743c60
danishanr <danishaanr@gmail.com>
2025-05-02 00:40:24 +0700

    Membuat access modifier private untuk fungsi luas dan keliling segitiga

--- tugastambahan.cpp
+++ tugastambahan.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 class Segitiga{
+    private :
+    int alas, tinggi, a, b, c;
+
+    float hitungLuas(){
+        return 0.5 * alas * tinggi;
+    }
+
+    float hitungKeliling(){
+        return (a + b + c);
+    }
 
 };
