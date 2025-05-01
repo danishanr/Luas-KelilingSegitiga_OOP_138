6c915bf581a5dfab044f34eb478d34a29636ec46
danishanr <danishaanr@gmail.com>
2025-05-02 00:43:21 +0700

    Membuat access modifier public untuk prosedur input data luas dan keliling segitiga

--- tugastambahan.cpp
+++ tugastambahan.cpp
@@ -12,5 +12,56 @@ class Segitiga{
     float hitungKeliling(){
         return (a + b + c);
     }
+        
+    public :
+    void inputDataLuas(){
+        do{
+            cout << "Masukkan alas segitiga (1 - 50) : ";
+            cin >> alas;
+
+            cout << "Masukkan tinggi segitiga (1 - 50 ): ";
+            cin >> tinggi;
+
+            if (alas < 1 || tinggi < 1 || alas > 50 || tinggi > 50){
+                cout << "Input tidak valid. Silahkan masukkan bilangan 1 - 50." << endl;
+            }
+            else if (alas% 2 != 0 || tinggi % 2 != 0){
+                cout << "Angka tersebut adalah bilangan ganjil, luas tidak dapat dihitung." << endl;
+            }
+            else{
+                hitungLuas();
+                break;
+            }  
+
+        } while (true);
+        cout << endl;
+        cout << endl;  
+    }
+    void inputDataKeliling(){
+        do{
+            cout << "Masukkan sisi a (1 - 50) : ";
+            cin >> a;
+
+            cout << "Masukkan sisi b (1 - 50 ): ";
+            cin >> b;
+
+            cout << "Masukkan sisi c (1 - 50) : ";
+            cin >> c;
+
+            if (a < 1 || b < 1 || c < 1 || a > 50 || b > 50 || c > 50){
+            cout << "Input tidak valid. Silahkan masukkan bilangan 1 - 50." << endl;
+            }
+            else if (a % 2 != 0 || b % 2 != 0 || c % 2 != 0){
+                cout << "Angka tersebut adalah bilangan ganjil. Keliling tidak dapat dihitung." << endl;
+            }
+            else{
+                hitungKeliling();
+                break;
+            }  
+        } while (true);
+        cout << endl;
+        cout << endl;  
+
+    }
 
 };
