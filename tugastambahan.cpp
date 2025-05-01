58185948c20700b78284c5c350e37cfbae79712a
danishanr <danishaanr@gmail.com>
2025-05-02 00:47:43 +0700

    Membuat access modifier public untuk prosedur output data luas dan keliling segitiga

--- tugastambahan.cpp
+++ tugastambahan.cpp
@@ -5,30 +5,37 @@ class Segitiga{
     private :
     int alas, tinggi, a, b, c;
 
-    float hitungLuas(){
+    float hitungLuas()
+    {
         return 0.5 * alas * tinggi;
     }
 
-    float hitungKeliling(){
+    float hitungKeliling()
+    {
         return (a + b + c);
     }
         
     public :
-    void inputDataLuas(){
-        do{
+    void inputDataLuas()
+    {
+        do
+        {
             cout << "Masukkan alas segitiga (1 - 50) : ";
             cin >> alas;
 
             cout << "Masukkan tinggi segitiga (1 - 50 ): ";
             cin >> tinggi;
 
-            if (alas < 1 || tinggi < 1 || alas > 50 || tinggi > 50){
+            if (alas < 1 || tinggi < 1 || alas > 50 || tinggi > 50)
+            {
                 cout << "Input tidak valid. Silahkan masukkan bilangan 1 - 50." << endl;
             }
-            else if (alas% 2 != 0 || tinggi % 2 != 0){
+            else if (alas% 2 != 0 || tinggi % 2 != 0)
+            {
                 cout << "Angka tersebut adalah bilangan ganjil, luas tidak dapat dihitung." << endl;
             }
-            else{
+            else
+            {
                 hitungLuas();
                 break;
             }  
@@ -37,8 +44,11 @@ class Segitiga{
         cout << endl;
         cout << endl;  
     }
-    void inputDataKeliling(){
-        do{
+
+    void inputDataKeliling()
+    {
+        do
+        {
             cout << "Masukkan sisi a (1 - 50) : ";
             cin >> a;
 
@@ -48,13 +58,16 @@ class Segitiga{
             cout << "Masukkan sisi c (1 - 50) : ";
             cin >> c;
 
-            if (a < 1 || b < 1 || c < 1 || a > 50 || b > 50 || c > 50){
+            if (a < 1 || b < 1 || c < 1 || a > 50 || b > 50 || c > 50)
+            {
             cout << "Input tidak valid. Silahkan masukkan bilangan 1 - 50." << endl;
             }
-            else if (a % 2 != 0 || b % 2 != 0 || c % 2 != 0){
+            else if (a % 2 != 0 || b % 2 != 0 || c % 2 != 0)
+            {
                 cout << "Angka tersebut adalah bilangan ganjil. Keliling tidak dapat dihitung." << endl;
             }
-            else{
+            else
+            {
                 hitungKeliling();
                 break;
             }  
@@ -64,4 +77,15 @@ class Segitiga{
 
     }
 
+    void outputData(int pilihan)
+    {
+        if (pilihan == 1)
+        {
+            cout << "Luas segitiga : " << hitungLuas() << endl;
+        }
+        else if (pilihan == 2)
+        {
+            cout << "Keliling segitiga : " << hitungKeliling() << endl;
+        }   
+    }
 };
